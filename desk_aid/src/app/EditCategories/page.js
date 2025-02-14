b7a7bfa18dd9834fec24961d2b065e966725fdce
Skia (Vic) <90403647+sccaeleo@users.noreply.github.com>
2025-02-14 03:02:22 -0500

    More attempts in setting up the database and backend
    https://github.com/sccaeleo/Desk-Aid/issues/1
    https://github.com/sccaeleo/Desk-Aid/issues/2

--- desk_aid/src/app/EditCategories/page.js
+++ desk_aid/src/app/EditCategories/page.js
@@ -1,8 +1,26 @@
+"use client"
+
+import React from "react";
+import { useState, useEffect } from "react";
+
 export default function Page() {
-return (
-    <div className="p-2">
-        <h1>Edit Guide Categories</h1>
-        
-    </div>
-);
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('api/server')
+            .then(response => response.text())
+            .then(data => setCategories(data));
+    }, []);
+
+    return (
+        <div className="p-2">
+            <h1>Edit Guide Categories</h1>
+            <button>Add Category</button>
+            <ul>
+                <p>{categories}</p>
+                
+            </ul>
+        </div>
+    );
 }
--- desk_aid/src/app/EditSelectPage/page.js
+++ desk_aid/src/app/EditSelectPage/page.js
@@ -5,17 +5,17 @@ export default function Page() {
         <div className="p-2">
             <h1>Edit Page</h1>
             <div className='w-full'>
-            <Link href="/" className="w-1/5">
+            <Link href="/EditResources" className="w-1/5">
                 <button className="hover:bg-blue-500 py-4 mx-2 w-1/5 flex-1 rounded">
                     Edit Resources
                 </button>
             </Link>
-            <Link href="/" className="w-1/5">
+            <Link href="/EditCategories" className="w-1/5">
                 <button className="hover:bg-blue-500 py-4 mx-2 w-1/5 flex-1 rounded">
                     Edit Guide Categories
                 </button>
             </Link>
-            <Link href="/" className="w-1/5">
+            <Link href="/EditGuides" className="w-1/5">
                 <button className="hover:bg-blue-500 py-4 mx-2 w-1/5 flex-1 rounded">
                     Edit Guides
                 </button>
