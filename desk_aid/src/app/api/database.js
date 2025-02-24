c911d6987913c2d7c9b481639f79c4a31d8f81cb
Skia (Vic) <90403647+sccaeleo@users.noreply.github.com>
2025-02-23 20:28:48 -0500

    database and server code
    https://github.com/sccaeleo/Desk-Aid/issues/1 https://github.com/sccaeleo/Desk-Aid/issues/2

--- desk_aid/src/app/api/database.js
+++ desk_aid/src/app/api/database.js
@@ -3,41 +3,40 @@ import sqlite3 from 'sqlite3'
 const db = new sqlite3.Database(
     "./database.db",
     sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+
+    // Check if error
     (err) => {
         if (err) {
         return console.error(err.message);
     }
-    console.log("Database connected.");
+
+    // Otherwise run database
+    else{
+        console.log("Database connected.");
+
+        // Create tables (if they don't already exist)
+        db.run(
+            `
+            CREATE TABLE IF NOT EXISTS categories (
+                id INTEGER PRIMARY KEY,
+                name TEXT  NOT NULL
+            );
+    
+            `
+        );
+        db.run(
+            `
+            CREATE TABLE IF NOT EXISTS resources (
+                id   INTEGER PRIMARY KEY,
+                name TEXT    NOT NULL
+            );
+            `
+        );
+
+        
+    }
+    
     }
 );
 
-db.serialize(function() {
-    db.run(
-        `
-        CREATE TABLE IF NOT EXISTS categories (
-            id INTEGER PRIMARY KEY,
-            name TEXT  NOT NULL
-        );
-
-        `
-    );
-    db.run(
-        `
-        CREATE TABLE IF NOT EXISTS resources (
-            id   INTEGER PRIMARY KEY,
-            name TEXT    NOT NULL
-        );
-        `
-    );
-
-});
-
-export default async function handler(req, res) {
-
-    // Temp
-    const rows = await db.all('SELECT * FROM categories');
-
-    res.status(200).json(rows);
-}
-
-db.close()
+export default db
--- desk_aid/src/app/api/server.js
+++ desk_aid/src/app/api/server.js
@@ -3,6 +3,8 @@ import express from 'express';
 const app = express();
 const port = 4000;
 
+import db from "./database.js"
+
 // Start server
 app.listen(port, () => {
     console.log("Server running.")
@@ -11,6 +13,38 @@ app.listen(port, () => {
 // Check server
 app.get("/", (req, res, next) => {
     res.json({"message":"The server is up and running!"})
+});
+
+// Get resources
+app.get("/api/resources", (req, res, next) => {
+    var sql = "select * from resources"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error":err.message});
+            return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+    });
+});
+
+// Get categories
+app.get("/api/categories", (req, res, next) => {
+    var sql = "select * from categories"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({"error":err.message});
+            return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+    });
 });
 
 // 404 Error on any other request
