9cd91a91beb0bb65cbc51757d8747d842af23987
Skia (Vic) <90403647+sccaeleo@users.noreply.github.com>
2025-03-01 23:51:11 -0500

    Tests updated to reflect changes

--- desk_aid/test.js
+++ desk_aid/test.js
@@ -5,20 +5,28 @@
 
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
+import axios from 'axios';
 import '@testing-library/jest-dom';
 
-
-
+// Page Imports
 import Home from './src/app/page.js';
 //import GuideList from './src/app/GuideListPage/page.js';
 //import Guide from './src/app/GuidePage/page.js';
-//import Resources from './src/app/ResourcesPage/page.js';
+import Resources from './src/app/ResourcesPage/page.js';
 //import Help from './src/app/HelpPage/page.js';
 //import SignIn from './src/app/SignInPage/page.js';
 
+jest.mock('axios');
+
 // Home Page Tests
 describe('Home page', () => {
-
+  axios.get.mockResolvedValue({
+    data: [
+      { name: 'Category 1' },
+      { name: 'Category 2' },
+      { name: 'Category 3' },
+    ],
+  });
   it('Displays title', () => {
     const { getByText } = render(<Home />);
     expect(getByText('Constituent Service Desk Aid')).toBeInTheDocument();
@@ -38,4 +46,22 @@ describe('Home page', () => {
     fireEvent.click(submitButton);
     waitFor(() => expect(searchMock).toHaveBeenCalledTimes(1));
   });
+});
+
+describe('Resources page', () => {
+  it('Renders resources', async () => {
+    const resources = [
+      { name: 'Resource 1' },
+      { name: 'Resource 2' },
+      { name: 'Resource 3' },
+    ];
+
+    axios.get.mockResolvedValue({ data: resources });
+
+    const { getByText } = render(<Resources />);
+    await waitFor(() => expect(getByText('Resource 1')).toBeInTheDocument());
+    await waitFor(() => expect(getByText('Resource 2')).toBeInTheDocument());
+    await waitFor(() => expect(getByText('Resource 3')).toBeInTheDocument());
+  });
+
 });
