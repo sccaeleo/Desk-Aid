c484c9739170f95614f4c42373214ed202342ac2
Skia (Vic) <90403647+sccaeleo@users.noreply.github.com>
2025-03-09 21:35:01 -0400

    reworking ui for sign in with tailwind form
    https://github.com/sccaeleo/Desk-Aid/issues/22

--- desk_aid/src/app/SignInPage/page.js
+++ desk_aid/src/app/SignInPage/page.js
@@ -1,6 +1,7 @@
 'use client'
 import Link from 'next/link'
 import { useState } from 'react';
+
 export default function Page() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -12,24 +13,34 @@ export default function Page() {
 
 return (
     <div>
-        <h1>Sign in</h1>
-        <form onSubmit={signIn}>
-            <label>Username: </label>
-            <input className='text-black w-4/5 rounded-l-md' 
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                placeholder="Username"
-            /> <br></br>
-            <label>Password: </label>
-            <input className='text-black w-4/5 rounded-l-md' 
-                type="text"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-            /><br></br>
-            <button type="submit" className='hover:bg-blue-500 rounded-r-md px-4'>Sign in</button>
+        <form class="max-w-sm mx-auto" onSubmit={signIn}>
+        <div class="mb-5">
+            <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username:</label>
+            <input 
+            type="username" 
+            id="username" 
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            required 
+            />
+        </div>
+        <div class="mb-5">
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password:</label>
+            <input
+            type="password" 
+            id="password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            required 
+            />
+        </div>
+        <button type="submit" class="hover:bg-blue-500 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
         </form>
+
     </div>
     
 );
